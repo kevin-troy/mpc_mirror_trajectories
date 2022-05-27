af01127611ae28381df88215a82b2479cc9aefd3
Kevin <90881154+kevin-troy@users.noreply.github.com>
2022-05-27 07:31:50 -0700

    Updated desc

--- mirror_dynamics.py
+++ mirror_dynamics.py
@@ -6,7 +6,7 @@ Simple continuous-time nonlinear dynamics for a pointing mirror
 
 def mirror_dynamics(s, u, R=0.5, Ixx=0.1, Iyy=0.1):
     """
-    Computes the state derivative from input state and control for a single axis of a pointing mirror.
+    Computes the state derivative from input state and control for a 2-axis pointing mirror.
 
     state = [tip, tilt, tip_dot, tilt_dot, ...etc]
     control = [F1, F2, F3, F4] where F1, F2 correspond to actuators about the tip axis and F3, F4 correspond to
